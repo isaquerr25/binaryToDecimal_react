4b13f9b268a5ade9ca29e2528f0bdd46735284cf
isaquerr25 <isaqueribeiro964@gmail.com>
2022-01-01 19:42:18 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ReactDOM  from 'react-dom';
+
+import {
+  StyledForm,
+  BinaryTextInput,
+  Label,
+  Button,
+  DecimalTextInput,
+  Field
+} from './Styled.js'
+
 
 function App() {
+
+  const [binaryText,setBinaryText]     = useState('');
+  const [decimalText,setDecimalText]    = useState('');
+  const [errorMensage,setErrorMansage] = useState('');
+
+  const onFormSubmit = e =>{
+    e.preventDefault() 
+
+    if(binaryText.match(/^[0-1]+$/g) === null ){
+      setErrorMansage('Enter either 0 or 1')
+      return
+    }
+  };
+
+  setErrorMansage('')
+  
+  const reverserBinaryText = binaryText.split('').map(Number).reverse()
+  
+
+  const result = reverserBinaryText.reduce(
+    (accumulator, currentValue, idx) =>
+      accumulator + currentValue * Math.pow(2, idx)
+  )
+  
+  setDecimalText(result);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Convert To Decimal</h1>
+      <StyledForm onSubmit={onFormSubmit}>
+        {errorMensage && <span style={{ color:'red'}} > {errorMensage} </span>}
+        <br />
+        <Field> 
+          <Label> Binary Input </Label>
+          <div>
+            <BinaryTextInput 
+              autoComplete='off'
+              type='text'
+              placeholder='Enter 0 or 1'
+              value={binaryText}
+              onChange={e => setBinaryText(e.target.value)}
+            />
+            <Button type='submit'>Convert</Button>
+          </div>
+        </Field>
+        <Field>
+          <Label>Decimal Output</Label>
+          <DecimalTextInput
+            type="text"
+            name="decimal"
+            value={decimalText}
+            disabled
+          />
+        </Field>
+      </StyledForm>
+    </>
   );
 }
 
-export default App;
+// export default App;
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />,rootElement)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM  from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {
+  StyledForm,
+  BinaryTextInput,
+  Label,
+  Button,
+  DecimalTextInput,
+  Field
+} from './Styled.js'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function App() {
+
+  const [binaryText,setBinaryText]     = useState('');
+  const [decimalText,setDecimalText]    = useState('');
+  const [errorMensage,setErrorMansage] = useState('');
+
+  const onFormSubmit = e =>{
+    e.preventDefault() 
+
+    if(binaryText.match(/^[0-1]+$/g) === null ){
+      setErrorMansage('Enter either 0 or 1')
+      return
+    }
+    
+
+    setErrorMansage('')
+    
+    const reverserBinaryText = binaryText.split('').map(Number).reverse()
+    
+
+    const result = reverserBinaryText.reduce(
+      (accumulator, currentValue, idx) =>
+        accumulator + currentValue * Math.pow(2, idx)
+    )
+    
+    setDecimalText(result);
+
+  };
+
+  return (
+    <>
+      <h1>Convert To Decimal</h1>
+      <StyledForm onSubmit={onFormSubmit}>
+        {errorMensage && <span style={{ color:'red'}} > {errorMensage} </span>}
+        <br />
+        <Field> 
+          <Label> Binary Input </Label>
+          <div>
+            <BinaryTextInput 
+              autoComplete='off'
+              type='text'
+              placeholder='Enter 0 or 1'
+              value={binaryText}
+              onChange={e => setBinaryText(e.target.value)}
+            />
+            <Button type='submit'>Convert</Button>
+          </div>
+        </Field>
+        <Field>
+          <Label>Decimal Output</Label>
+          <DecimalTextInput
+            type="text"
+            name="decimal"
+            value={decimalText}
+            disabled
+          />
+        </Field>
+      </StyledForm>
+    </>
+  );
+}
+
+// export default App;
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />,rootElement)
